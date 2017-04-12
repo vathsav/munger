4385a11d70c04a086adf70f1a3471062487c6e0a
vathsav <vathsav@live.in>
2017-04-12 23:43:07 +0200

    feat: pushing more metadata

--- munger.js
+++ munger.js
@@ -46,6 +46,7 @@ var displaySelector = function(titles) {
     reader.question('\nEnter the number of records to parse: ', (count, err) => {
       if (err)
         console.log(err);
+
       constructJSON(columnData.length, choices);
       reader.close();
     });
@@ -58,7 +59,7 @@ var displaySelector = function(titles) {
   list.list();
 }
 
-fs.readFile('data/data.csv', 'utf8', function (err, data) {
+fs.readFile('data/data_complete.csv', 'utf8', function (err, data) {
   if(err) spinner.fail('Failed to parse data');
   spinner.succeed('Data read successfully');
   columnData = d3.csvParseRows(data);
@@ -73,19 +74,50 @@ fs.readFile('data/data.csv', 'utf8', function (err, data) {
 });
 
 function constructJSON(count, choices) {
+  // AUTORE/I	AA	DATA	RELATORE	CORRELATORE	CORSO DI STUDI	FACOLTA'/SCUOLA	ACCESSO FILE	TIPO LAUREA	HANDLE	LINGUA	PAROLE CHIAVE - IT	PAROLE CHIAVE - EN	SSD	TITOLO	TIPO TESI
+  // TODO: Temporary variables. Must be made dynamic based on choices
+  var listOfAuthors = [];
+  var listOfAcademicYears = [];
+  var listOfDates = [];
+  var listOfRelatores = [];
+  var listOfCorrelators = [];
   var listOfCourses = [];
+  var listOfSchools = [];
+  var listOfAccesses = [];
+  var listOfDegreeTypes = [];
+  var listOfHandles = [];
+  var listOfLanguages = [];
   var listOfKeywords = [];
+  var listOfKeywordsEN = [];
+  var listOfSSDs = []; // ???
+  var listOfTitles = [];
+  var listOfTypesOfTheses = [];
 
   for (var i = 0; i < count; i++) {
-    if (columnData[i][1].includes(',')) {
+    if (columnData[i][9].includes(',')) {
       // Make sure
-      columnData[i][1].replace(',', ';');
+      columnData[i][9].replace(',', ';');
     }
-    listOfCourses.push(columnData[i][0]);
-    listOfKeywords.push(columnData[i][1]);
+
+    listOfAuthors.push(columnData[i][0]);
+    listOfAcademicYears.push(columnData[i][1]);
+    listOfDates.push(columnData[i][2]);
+    listOfRelatores.push(columnData[i][3]);
+    listOfCorrelators.push(columnData[i][4]);
+    listOfCourses.push(columnData[i][5]);
+    listOfSchools.push(columnData[i][6]);
+    listOfAccesses.push(columnData[i][7]);
+    listOfDegreeTypes.push(columnData[i][8]);
+    listOfHandles.push(columnData[i][9]);
+    listOfLanguages.push(columnData[i][10]);
+    listOfKeywords.push(columnData[i][11]); // Cus that's how it is
+    listOfKeywordsEN.push(columnData[i][12]);
+    listOfSSDs.push(columnData[i][13]); // ???
+    listOfTitles.push(columnData[i][14]);
+    listOfTypesOfTheses.push(columnData[i][15]);
   }
 
-  var keywords = {};
+  var keys = {};
   // Omit the titles of the columns. Iterate from index 1.
   for (var i = 1; i < count; i++) {
     var course = listOfCourses[i].toLowerCase();
@@ -94,42 +126,86 @@ function constructJSON(count, choices) {
     if (course.length == 0)
       course = 'Error';
 
-    arrayOfKeywords.forEach(function(keyword, index) {
+    arrayOfKeywords.forEach(function(key, index) {
       // Ditch invalid characters - Keys must be non-empty strings and can't contain ".", "#", "$", "/", "[", or "]"
-      if (keyword.length == 0 || keyword.length > 100)
-        keyword = "Error";
+      if (key.length == 0 || key.length > 100)
+        key = "Error";
 
       invalidCharacters.forEach(function(character, index) {
-        for (var position = 0; position < keyword.length; position++) {
-          if (keyword.includes(character)) {
-            keyword = keyword.replace(character, "");
+        for (var position = 0; position < key.length; position++) {
+          if (key.includes(character)) {
+            key = key.replace(character, "");
           }
         }
       });
 
-      // Check if keyword is already present in the object
-      if (keywords[keyword] != undefined) {
-        // Check if the course has already been encountered for this keyword
-        var keywordObject = JSON.stringify(keywords[keyword]);
-        keywordObject = keywordObject.substring(1, keywordObject.length - 1);
+      // Check if key is already present in the object
+      if (keys[key] != undefined) {
+        // Check if data already been encountered for this key
+        var object = JSON.stringify(keys[key]);
+        object = object.substring(1, object.length - 1);
 
-        if (keywords[keyword][course] != undefined) {
-          // The course has already been encountered for this keyword. So get the count, and increment it.
-          var count = keywords[keyword][course] + 1;
-          keywords[keyword] = JSON.parse('{ ' + keywordObject + ', "' + course + '": ' + count + ' }');
+        if (keys[key][course] != undefined) {
+          // The course has already been encountered for this key. So get the count, and increment it.
+          var count = keys[key][course] + 1;
+          keys[key] = JSON.parse('{ ' + object + ', "' + course + '": ' + count
+              + ', "author": "' + listOfAuthors[i]
+              // + '", "academic_year": "' + listOfAcademicYears[i]
+              // + '", "date": "' + listOfDates[i]
+              // + '", "relators": "' + listOfRelatores[i]
+              // + '", "correlators": "' + listOfCorrelators[i]
+              // + '", "access": "' + listOfAccesses[i]
+              // + '", "degree_type": "' + listOfDegreeTypes[i]
+              // + '", "handle": "' + listOfHandles[i]
+              // + '", "keyword_it": "' + listOfKeywordsEN[i]
+              // + '", "ssd": "' + listOfSSDs[i]
+              // + '", "title": "' + listOfTitles[i]
+              + '", "type": "' + listOfTypesOfTheses[i]
+              + '"}'
+            );
         } else {
           // First encounter for a particular course
-          keywords[keyword] = JSON.parse('{ ' + keywordObject + ', "' + course + '": ' + 1 + ' }');
+          keys[key] = JSON.parse('{ ' + object + ', "' + course + '": ' + 1
+            + ', "author": "' + listOfAuthors[i]
+            // + '", "academic_year": "' + listOfAcademicYears[i]
+            // + '", "date": "' + listOfDates[i]
+            // + '", "relators": "' + listOfRelatores[i]
+            // + '", "correlators": "' + listOfCorrelators[i]
+            // + '", "access": "' + listOfAccesses[i]
+            // + '", "degree_type": "' + listOfDegreeTypes[i]
+            // + '", "handle": "' + listOfHandles[i]
+            // + '", "keyword_it": "' + listOfKeywordsEN[i]
+            // + '", "ssd": "' + listOfSSDs[i]
+            // + '", "title": "' + listOfTitles[i]
+            + '", "type": "' + listOfTypesOfTheses[i]
+            + '"}'
+          );
         }
       } else {
-        // Encountering the keyword for the first time
-        keywords[keyword] = JSON.parse('{ "' + course + '": ' + 1 + '}');
+        // Encountering the key for the first time
+        keys[key] = JSON.parse('{ "' + course + '": ' + 1
+            + ', "author": "' + listOfAuthors[i]
+            // + '", "academic_year": "' + listOfAcademicYears[i]
+            // + '", "date": "' + listOfDates[i]
+            // + '", "relators": "' + listOfRelatores[i]
+            // + '", "correlators": "' + listOfCorrelators[i]
+            // + '", "access": "' + listOfAccesses[i]
+            // + '", "degree_type": "' + listOfDegreeTypes[i]
+            // + '", "handle": "' + listOfHandles[i]
+            // + '", "keyword_it": "' + listOfKeywordsEN[i]
+            // + '", "ssd": "' + listOfSSDs[i]
+            // + '", "title": "' + listOfTitles[i]
+            + '", "type": "' + listOfTypesOfTheses[i]
+            + '"}'
+          );
       }
 
-      keywords[keyword].total = 0;
-      keywords[keyword].total = d3.sum(d3.values(keywords[keyword]));
+      keys[key].total = 0;
+      keys[key].total = d3.sum(d3.values(keys[key]));
     });
   }
 
-  utils.push('/keywords', keywords);
+  // TODO: Promt Firebase reference to push to
+
+  utils.push('/keywords', keys);
 }
